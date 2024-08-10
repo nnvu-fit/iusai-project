c9c99f1a6cbbb42764a430be873c8ab6ccaa78ca
Nguyễn Ngọc Vũ <25165348+nnvu-fit@users.noreply.github.com>
2024-08-10 17:19:21 +0700

    .

--- model.py
+++ model.py
@@ -32,38 +32,41 @@ class CNN(nn.Module):
     def __init__(self, num_classes):
         super(CNN, self).__init__()
         self.num_classes = num_classes
-        self.features = nn.Sequential(
-            # Layer 1
-            nn.Conv2d(3, 64, kernel_size=(3,3), stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(3,3), stride=2),
-            nn.SyncBatchNorm(),
-            # Layer 2
-            nn.Conv2d(64, kernel_size=(3,3)),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(3,3), stride=2),
-            nn.SyncBatchNorm(),
-            # Layer 3
-            nn.Conv2d(64, 96, kernel_size=(3,3)),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(3,3), stride=2),
-            nn.SyncBatchNorm(),
-            # Layer 4
-            nn.Conv2d(96, 32, kernel_size=(3,3)),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(2,2), stride=2),
-            nn.SyncBatchNorm(),
-            nn.Dropout(0.2),
-            nn.Flatten(),
-            # Layer 5
-            nn.Linear(32*6*6, 128),
-            nn.ReLU(inplace=True),
-            # Layer 6
-            nn.Linear(128, num_classes),
-            nn.Softmax(dim=1)
-        )
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=(3,3), stride=4, padding=2)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=(3,3))
+        self.conv3 = nn.Conv2d(64, 96, kernel_size=(3,3))
+        self.conv4 = nn.Conv2d(96, 32, kernel_size=(3,3))
+        self.fc1 = nn.Linear(32*6*6, 128)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
-        x = self.features(x)
+        # Layer 1
+        x = self.conv1(x)
+        x = nn.ReLU(inplace=True)(x)
+        x = nn.MaxPool2d(kernel_size=(3,3), stride=2)(x)
+        x = nn.BatchNorm2d(64)(x)
+        # Layer 2
+        x = self.conv2(x)
+        x = nn.ReLU(inplace=True)(x)
+        x = nn.MaxPool2d(kernel_size=(3,3), stride=2)(x)
+        x = nn.BatchNorm2d(64)(x)
+        # Layer 3
+        x = self.conv3(x)
+        x = nn.ReLU(inplace=True)(x)
+        x = nn.MaxPool2d(kernel_size=(3,3), stride=2)(x)
+        x = nn.BatchNorm2d(96)(x)
+        # Layer 4
+        x = self.conv4(x)
+        x = nn.ReLU(inplace=True)(x)
+        x = nn.MaxPool2d(kernel_size=(2,2), stride=2)(x)
+        x = nn.BatchNorm2d(32)(x)
+        x = nn.Dropout(0.2)(x)
+        x = nn.Flatten()(x)
+        # Layer 5
+        x = self.fc1(x)
+        x = nn.ReLU(inplace=True)(x)
+        # Layer 6
+        x = self.fc2(x)
+        x = nn.Softmax(dim=1)(x)
         
         return x
